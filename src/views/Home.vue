230be11102792407c3af7437428b12848b0caa9d
eliko <elikohon@gmail.com>
2021-06-18 10:46:28 -0300

    Revert "Update Home.vue"
    This reverts commit c66165358589ab6469f8b7a8690c2454e136bb5f.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -84,14 +84,12 @@ export default {
             headers: {
               "Access-Control-Allow-Origin": "*",
               "Access-Control-Allow-Methods": "GET",
-              "Access-Control-Allow-Headers": "*",
-              "Access-Control-Expose-Headers": "Content-Length, X-JSON"
+              "Access-Control-Allow-Headers": "*"
             }
           });
           axiosInstance
             .get( 'https://iawek-servicio-web.herokuapp.com/recetas')
             .then(response => (this.recetas = response.data))
-            
       }
        
     }
