824816d3df4b579c72974aa4287345d9658d4635
eliko <elikohon@gmail.com>
2021-06-26 19:40:07 -0300

    PWA

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import { BootstrapVue } from 'bootstrap-vue'
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './registerServiceWorker'
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
